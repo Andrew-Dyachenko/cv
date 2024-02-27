d1bebe2b913726ef73496c5f5d2d528c9c610ed6
Andrew-Dyachenko <north.inhale@gmail.com>
2024-02-28 02:04:01 +0400

    feat: set Nunito Sans font
    Change Latin font to Nunito Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "CV - Andrey Dyacheko",
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={nunitoSans.className}>{children}</body>
+		</html>
+	);
 }
