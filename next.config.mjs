d2724432d22337a276e6956f8c63c15c90e32396
Andrew-Dyachenko <north.inhale@gmail.com>
2024-03-01 05:33:11 +0400

    feat: avif & device sizes
    Configure Next.js to support Avif image format. Device sizes reduces to 320, 600, 768

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		deviceSizes: [320, /*375, 480,*/ 600, 768, /*924, 1272, 1600, 1920*/],
+		formats: ['image/avif', 'image/webp'],
+		// minimumCacheTTL: 60 * 60 * 24 * 365, // One year
+	},
+};
 
 export default nextConfig;
