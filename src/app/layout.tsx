568e0470e52da3a3d51b4f71e3bfff44c086548e
Andrew-Dyachenko <north.inhale@gmail.com>
2024-03-03 07:24:06 +0400

    feat: sofia sans font seted
    Set Sofia Sans font instead of the Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,38 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.scss";
+import {
+	// Inter,
+	// Ubuntu,
+	// Open_Sans,
+	// Lora,
+	// Lato,
+	// IBM_Plex_Sans,
+	// Quicksand,
+	// Nunito,
+	// Rubik,
+	// Comfortaa,
+	// Montserrat_Alternates,
+	// Jura,
+	Sofia_Sans,
+	// Ysabeau,
+} from "next/font/google";
 import clsx from "clsx";
 
 import DirectionToggle from "@/components/DirectionToggle";
+const font = Sofia_Sans({
+	weight: [
+		// "100",
+		// "200",
+		// "300",
+		"400",
+		"500",
+		"600",
+		"700",
+		// "800",
+		// "900",
+		// "1000",
+	],
+	subsets: ["latin", /*"cyrillic"*/],
+});
 
 export const metadata: Metadata = {
 	title: `CV - ${author}`,
