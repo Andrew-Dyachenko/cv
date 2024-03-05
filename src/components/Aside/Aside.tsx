6028d6427b14aca4821bddce51bd4d3bcdfed53e
Andrew-Dyachenko <north.inhale@gmail.com>
2024-03-05 04:37:12 +0400

    feat(abbr): underline decoration
    Set no underline decoration at @print

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -28,7 +28,7 @@ export default function Aside() {
 					Frontend developer
 				</span>{" "}
 				<span className={styles["aside__occupation-level"]}>
-					<abbr title="A Middle Frontend developer typically has experience and skills covering: 1. Layout (HTML, CSS); 2. JavaScript and frameworks; 3. Working with RESTful API; 4. State management; 5. Adaptive and responsive layout; 6. Testing; 7.Performance optimization; 8.Versioning and assembly; 9.Communication and cooperation; 10. Continuous training;">
+					<abbr className={styles["aside__occupation-abbr"]} title="A Middle Frontend developer typically has experience and skills covering: 1. Layout (HTML, CSS); 2. JavaScript and frameworks; 3. Working with RESTful API; 4. State management; 5. Adaptive and responsive layout; 6. Testing; 7.Performance optimization; 8.Versioning and assembly; 9.Communication and cooperation; 10. Continuous training;">
 						(middle/middle+)
 					</abbr>
 				</span>
