7d6340f34b2d60b795c6f26ca38bbb8571f0a0c7
Andrew-Dyachenko <north.inhale@gmail.com>
2024-03-05 21:43:57 +0400

    fix(experience): occupation level text
    Missing spaces

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -40,7 +40,7 @@ export default function Aside() {
 				<span className={styles.occupation__level}>
 					<abbr
 						className={styles.occupation__abbr}
-						title="A Middle Frontend developer typically has experience and skills covering: 1. Layout (HTML, CSS); 2. JavaScript and frameworks; 3. Working with RESTful API; 4. State management; 5. Adaptive and responsive layout; 6. Testing; 7.Performance optimization; 8.Versioning and assembly; 9.Communication and cooperation; 10. Continuous training;"
+						title="A Middle Frontend developer typically has experience and skills covering: 1. Layout (HTML, CSS); 2. JavaScript and frameworks; 3. Working with RESTful API; 4. State management; 5. Adaptive and responsive layout; 6. Testing; 7. Performance optimization; 8. Versioning and assembly; 9. Communication and cooperation; 10. Continuous training;"
 					>
 						(middle / middle+)
 					</abbr>
