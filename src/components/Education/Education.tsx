ac92812db61cdbc3837379d61bfa5d951aba9523
Andrew-Dyachenko <north.inhale@gmail.com>
2024-03-07 10:32:19 +0400

    feat(education): draft improved
    Content and styles added

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,5 +1,6 @@
 import styles from "./Education.module.scss";
 import { IoSchoolOutline } from "react-icons/io5";
+import getFormattedDateTime from "@/utils/getFormattedDateTime";
 
 export default function Education() {
 	return (
@@ -9,6 +10,72 @@ export default function Education() {
 				<IoSchoolOutline className={styles["education__title-icon"]} />
 				Education
 			</h3>
+			<ul className={styles.education__list}>
+				<li className={styles.education__item}>
+					<h4 className={styles.education__where}>
+						Krasnoyarsk Assembly College
+					</h4>
+					<div className={styles.education__degree}>
+						Welding technologies - Bachelor
+					</div>
+					<div className={styles.education__when}>
+						(
+						<time
+							dateTime={getFormattedDateTime({
+								year: 2007,
+								month: 9,
+								day: 1,
+								timezoneOffset: 60 * 7,
+							})}
+						>
+							2007
+						</time>{" "}
+						-{" "}
+						<time
+							dateTime={getFormattedDateTime({
+								year: 2010,
+								month: 6,
+								day: 25,
+								timezoneOffset: 60 * 7,
+							})}
+						>
+							2010
+						</time>
+						)
+					</div>
+				</li>
+				<li className={styles.education__item}>
+					<h4 className={styles.education__where}>
+						Siberian State Aerospace University <small>(Nonprofit Partnership Graduate School of Business)</small>
+					</h4>
+					<div className={styles.education__degree}>Programmer - Bachelor</div>
+					<div className={styles.education__when}>
+						(
+						<time
+							dateTime={getFormattedDateTime({
+								year: 2004,
+								month: 9,
+								day: 1,
+								timezoneOffset: 60 * 7,
+							})}
+						>
+							2004
+						</time>{" "}
+						-{" "}
+						<time
+							dateTime={getFormattedDateTime({
+								year: 2006,
+								month: 4,
+								day: 14,
+								timezoneOffset: 60 * 7,
+							})}
+						>
+							2006
+						</time>
+						)
+					</div>
+				</li>
+			</ul>
 		</section>
 	);
 }
