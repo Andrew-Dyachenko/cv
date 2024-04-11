0ed8049c848e374220db66891d4ec66c0893a9e4
Andrew-Dyachenko <north.inhale@gmail.com>
2024-04-12 02:06:57 +0400

    feat(header): display version

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -3,13 +3,16 @@ import clsx from "clsx";
 import Link from "next/link";
 // import { Html } from "next/document";
 import FontSize from "./FontSize";
+import packageJSON from "../../../../package.json";
+
+const { version } = packageJSON;
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
 			<div className={clsx("container", styles.header__container)}>
 				<Link href="/" className={styles.brand}>
-					CV
+					CV <sub className={styles.brand__version}>v{version}</sub>
 				</Link>
 				<FontSize />
 			</div>
