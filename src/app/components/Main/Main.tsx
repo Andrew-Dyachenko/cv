631135849d7ed2f65b06e6654969848fe9f7fedf
Andrew-Dyachenko <north.inhale@gmail.com>
2025-02-24 16:47:51 +0300

    fix: summary
    ✅ Removed "Specializes in" → now sounds like a confident Statement.
    ✅ Added about technologies (React, Next.js, Vue, Angular).
    ✅ Added about Storybook and design systems.
    ✅ Clarified about email layout (Outlook support).
    ✅ Added a phrase about teamwork.
    ✅ Wrapped the abbreviation of the NPM in the abbr tag with title.

--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -12,7 +12,9 @@ export default function Main() {
 				</h2>
 				<hr className={styles.section__hr} />
 				<div className={styles.section__body}>
-					Specializes in the development of web applications, including:{" "}
+					<p>
+						Experienced in developing modern web applications, including:
+					</p>
 					<ul>
 						<li>
 							<span>
@@ -30,13 +32,23 @@ export default function Main() {
 							</span>
 						</li>
 						<li>Landing Pages</li>
-						<li>NPM Packages</li>
-						<li>Emails</li>
+						<li>
+							<abbr title="Node Package Manager">NPM</abbr> Packages
+						</li>
+						<li>Emails (transactional & marketing)</li>
 					</ul>
-					Has the ability to create adaptive, responsive, accessible, valid,
-					stable, cross-browser, pixel-perfect, user interfaces.
+					<p>
+						Proficient in building adaptive, responsive, accessible, valid, and
+						cross-browser user interfaces using modern frontend technologies such as{" "}
+						(<strong>React, Next.js, Vue, Angular</strong>). Skilled in working with{" "}
+						<strong>design systems</strong>, component libraries (<strong>Storybook</strong>), and
+						ensuring compatibility across all major email clients, including (<strong>Outlook</strong> <code style={{ fontSize: "0.75em" }}>^2007</code>).
+						Experienced in collaborative development, working closely with designers,
+						developers, and content teams to deliver high-quality UI solutions.
+					</p>
 				</div>
 			</section>
+
 			<section className={styles.section}>
 				<h2 className={styles.section__title}>
 					<AiTwotoneExperiment className={styles["section__title-icon"]} /> Work
